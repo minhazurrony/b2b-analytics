9ba17457e519b65026d309a5a07b27bd1bb03f08
Minhazur Rahman <minhazur.rony@gmail.com>
2025-05-28 23:54:28 +0600

    chore: add poppins font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,8 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const poppins = Poppins({
+  weight: ["300", "400", "500", "600"],
   subsets: ["latin"],
 });
 
@@ -19,11 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={poppins.className}>{children}</body>
     </html>
   );
 }

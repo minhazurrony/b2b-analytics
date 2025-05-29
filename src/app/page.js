925fca23755353600c1be21a771f614edbe9b88c
Minhazur Rahman <minhazur.rony@gmail.com>
2025-05-29 19:59:28 +0600

    feat: add container component and complete static kpi results section

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
+import { Container } from "@/components/custom/container";
 import { KPIResultTable } from "../components/custom/kpi-result-table";
 
 export default function Home() {
   return (
     <>
-      <KPIResultTable />
+      <Container title="KPI Results">
+        <KPIResultTable />
+      </Container>
     </>
   );
 }

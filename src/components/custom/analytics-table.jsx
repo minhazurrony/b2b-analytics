421e6600caaddbe9b4d30552be7c19e3232dd5e2
Minhazur Rahman <minhazur.rony@gmail.com>
2025-05-30 21:43:09 +0600

    feat: add revenue benchmark analytic table with api integration

---
+++ src/components/custom/analytics-table.jsx
@@ -0,0 +1,42 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+export const AnalyticsTable = ({ columns, data, className = "" }) => {
+  return (
+    <div
+      className={`rounded-sm p-5 border border-dime-outline-grey bg-white overflow-auto ${className}`}>
+      <Table>
+        <TableHeader className="border-b-dime-body-grey">
+          <TableRow>
+            {columns.map((col) => (
+              <TableHead
+                key={col.key}
+                className="text-dime-dark-grey text-sm font-medium uppercase">
+                {col.label}
+              </TableHead>
+            ))}
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {data.map((row, idx) => (
+            <TableRow key={idx} className="even:bg-muted/20">
+              {columns.map((col) => (
+                <TableCell
+                  key={col.key}
+                  className="font-light text-sm text-dime-dark-grey pt-5">
+                  {row[col.key]}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+};

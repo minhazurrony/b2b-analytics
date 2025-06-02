189b9cd1d601c0f2ec68935121658d4cd5817b10
Minhazur Rahman <minhazur.rony@gmail.com>
2025-06-02 09:16:28 +0600

    feat: add bottom sidebar menu

--- src/components/custom/app-sidebar.jsx
+++ src/components/custom/app-sidebar.jsx
@@ -4,61 +4,79 @@ import {
   CreditCard,
   Mails,
   FileCheck,
+  Settings,
+  CookingPot,
 } from "lucide-react";
 import React from "react";
-import { ScrollArea } from "@/components/ui/scroll-area";
 import { Logo } from "@/assets/images/logo";
 
-// Menu items.
-const items = [
-  {
-    title: "Dashboard",
-    url: "#",
-    icon: LayoutDashboard,
-  },
-  {
-    title: "Launchpad",
-    url: "#",
-    icon: Rocket,
-  },
-  {
-    title: "Payments",
-    url: "#",
-    icon: CreditCard,
-  },
-  {
-    title: "Messages",
-    url: "#",
-    icon: Mails,
-  },
-  {
-    title: "Tasks",
-    url: "#",
-    icon: FileCheck,
-  },
-];
+const MENU_ITEMS = {
+  top: [
+    {
+      title: "Dashboard",
+      url: "#",
+      icon: LayoutDashboard,
+    },
+    {
+      title: "Launchpad",
+      url: "#",
+      icon: Rocket,
+    },
+    {
+      title: "Payments",
+      url: "#",
+      icon: CreditCard,
+    },
+    {
+      title: "Messages",
+      url: "#",
+      icon: Mails,
+    },
+    {
+      title: "Tasks",
+      url: "#",
+      icon: FileCheck,
+    },
+  ],
+
+  bottom: [
+    {
+      title: "Settings",
+      url: "#",
+      icon: Settings,
+    },
+    {
+      title: "Launchpad",
+      url: "#",
+      icon: CookingPot,
+    },
+  ],
+};
+
+const MenuItemsContainer = ({ items }) => (
+  <div className="space-y-1.5">
+    {items.map(({ title, url, icon: Icon }) => (
+      <a
+        key={title}
+        href={url}
+        className="flex items-center gap-2 rounded p-2 text-xs font-semibold text-dime-field-grey hover:bg-dime-light-purple">
+        <Icon className="w-4 h-4" />
+        <span>{title}</span>
+      </a>
+    ))}
+  </div>
+);
 
 export const AppSidebar = () => {
   return (
     <aside
-      className="w-[176px] h-full hidden md:block"
+      className="w-[176px] h-full hidden md:block p-4"
       style={{ background: "var(--color-dime-purple-blue-gradient)" }}>
-      <ScrollArea className="h-full p-4">
-        <Logo />
-        <div className="mt-8 space-y-1.5">
-          {items.map((item) => {
-            return (
-              <a
-                key={item.title}
-                href={item.url}
-                className="flex rounded p-2 text-center justify-start items-center gap-2 text-dime-field-grey text-xs font-semibold leading-[150%] hover:bg-dime-light-purple">
-                <item.icon className="w-4 h-4" />
-                <span>{item.title}</span>
-              </a>
-            );
-          })}
-        </div>
-      </ScrollArea>
+      <Logo />
+      <div className="mt-8 flex h-[calc(100vh-5rem)] flex-col justify-between">
+        <MenuItemsContainer items={MENU_ITEMS.top} />
+        <MenuItemsContainer items={MENU_ITEMS.bottom} />
+      </div>
     </aside>
   );
 };
